fb15181caf8cae2c9a2a7f646f939a3e3df2e3b7
Pol Maresch <polmaresch@gmail.com>
2024-02-27 22:34:59 +0100

    Fix

--- TFG-Dimensions-Game/Assets/Scripts/Renderer2D.cs
+++ TFG-Dimensions-Game/Assets/Scripts/Renderer2D.cs
@@ -6,6 +6,8 @@ public class Renderer2D : MonoBehaviour
 {
     // Start is called before the first frame update
     public float rayLength = 10f;
+    RaycastHit[] hits;
+    List<RaycastHit> allHits = new();
     void Start()
     {
     }
@@ -13,16 +15,32 @@ public class Renderer2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit[] hits = Physics.RaycastAll(transform.position, transform.forward, rayLength);
-        foreach (RaycastHit hit in hits)
-        {
-            hit.collider.gameObject.layer = LayerMask.NameToLayer("2D");
-            Debug.Log("El rayo ha golpeado a: " + hit.collider.gameObject.name);
-        }
-        else
-        {
 
+        RaycastHit[] actualHits = Physics.RaycastAll(transform.position, transform.forward, rayLength);
+
+        if (hits != actualHits)
+        {
+            hits = actualHits;
+            foreach (RaycastHit hit in hits)
+            {
+                hit.collider.gameObject.layer = LayerMask.NameToLayer("2D");
+
+                allHits.Add(hit);
+                Debug.Log("Entro");
+            }
         }
+
+       
+
+
+        //for (int i = 0; i < hits.Length; i++)
+        //{
+        //    if (allHits[i] != hits[i] )
+        //    {
+        //
+        //    }
+        //}
+
     }
 
     private void OnDrawGizmos()
