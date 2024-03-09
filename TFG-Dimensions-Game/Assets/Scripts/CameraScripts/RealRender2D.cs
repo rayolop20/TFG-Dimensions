25b5c552e844900f7a90ea723b9901b438906efb
Pol Maresch <polmaresch@gmail.com>
2024-03-09 01:38:20 +0100

    Afegint Llista de objectes golpejats

--- TFG-Dimensions-Game/Assets/Scripts/CameraScripts/RealRender2D.cs
+++ TFG-Dimensions-Game/Assets/Scripts/CameraScripts/RealRender2D.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-
-public class StopRayOnCollision : MonoBehaviour
+public class RealRender2D : MonoBehaviour
 {
+    struct HitObjects
+    {
+        public string name;
+        public Vector3 initPosition;
+        public Vector3 endPosition;
+    }
+
     public float rayLength = 10f; // Longitud del rayo
     public LayerMask layerMask; // Máscara de capas para la detección de colisiones
     public GameObject plane;
@@ -16,6 +21,7 @@ public class StopRayOnCollision : MonoBehaviour
     int hitsCount = 0;
     Vector3 LastHitPosition;
     List<Vector3> positions = new();
+    List<HitObjects> hObjetcs = new();
     private void Start()
     {
         // Comprobar si el rayo colisiona con algo
@@ -23,8 +29,6 @@ public class StopRayOnCollision : MonoBehaviour
     }
     void Update()
     {
-
-
         positions.Clear();
         LastHitPosition = transform.position;
 
@@ -34,34 +38,49 @@ public class StopRayOnCollision : MonoBehaviour
 
             RaycastHit hitInfo;
 
+
             if (Physics.Raycast(ray, out hitInfo, rayLength))
             {
                 hitsCount++;
-                LastHitPosition = hitInfo.point + new Vector3(0, 0, 0.01f);
-                Debug.Log("Punto de salida: " + hitInfo.point);
-
                 positions.Add(hitInfo.point);
+
+                LastHitPosition = hitInfo.point + new Vector3(0, 0, 0.01f);
+
                 Debug.Log(positions[i]);
                 // Dibujar el rayo en la escena hasta el punto de colisión
                 Debug.DrawRay(LastHitPosition, transform.forward * hitInfo.distance, Color.red);
 
             }
+
+            if (i % 2 == 0 && i != 0)
+            {
+                HitObjects colidedObject = new HitObjects();
+
+                colidedObject.initPosition = positions[i - 1];
+                colidedObject.endPosition = positions[i - 2];
+
+                hObjetcs.Add(colidedObject);
+            }
+
         }
 
-        //calcular escala i posicio del pla
-        if (positions.Count > 0 && instant == 0)
+        foreach (HitObjects item in hObjetcs)
         {
-            p = Instantiate(plane, new Vector3(0, 0, 0), Quaternion.identity);
+            GameObject nPlane = new GameObject(); 
+            nPlane = Instantiate(plane, new Vector3(0, 0, 0), Quaternion.identity);
         }
-        float scale = (positions[1].z - positions[0].z);
-        float position = scale / 2;
-
-
-
-        p.layer = LayerMask.NameToLayer("2D");
-        p.transform.position = new Vector3(0, 0, positions[0].z + position);
-        p.transform.localScale = new Vector3(plane.transform.localScale.x / 10, 1, scale / 10);
-        instant++;
+        
+       // //calcular escala i posicio del pla
+       //    
+       // float scale = (positions[1].z - positions[0].z);
+       // float position = scale / 2;
+       //
+       //
+       //
+       // p.layer = LayerMask.NameToLayer("2D");
+       // p.transform.position = new Vector3(0, 0, positions[0].z + position);
+       // p.transform.localScale = new Vector3(plane.transform.localScale.x / 10, 1, scale / 10);
+       // instant++;
 
     }
 
