7243cb4d0ad8cc49c163fa0bf3c2261c1e9f5f23
Pol Maresch <79161063+rayolop20@users.noreply.github.com>
2024-04-09 09:59:24 +0200

    Hits Count???

--- TFG-Dimensions-Game/Assets/Scripts/CameraScripts/RealRender2D.cs
+++ TFG-Dimensions-Game/Assets/Scripts/CameraScripts/RealRender2D.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 
 public class RealRender2D : MonoBehaviour
 {
-    struct HitObjects
+    public struct HitObjects
     {
-        public GameObject nPlanes;
         public Vector3 initPosition;
         public Vector3 endPosition;
     }
 
     public float rayLength = 10f; // Longitud del rayo
     public LayerMask layerMask; // Máscara de capas para la detección de colisiones
-    public GameObject plane;
-    public GameObject p;
+
 
     int hitsCount = 0;
     Vector3 LastHitPosition;
-    List<Vector3> positions = new();
-    List<HitObjects> hObjetcs = new();
+    private List<Vector3> newPositions = new();
+    private List<Vector3> lastPositions = new();
+    public List<HitObjects> hObjetcs = new();
     private void Start()
     {
         // Comprobar si el rayo colisiona con algo
@@ -27,11 +26,11 @@ public class RealRender2D : MonoBehaviour
     }
     void Update()
     {
-        positions.Clear();
+        newPositions.Clear();
         hitsCount = 0;
         LastHitPosition = transform.position;
 
-        for (int i = 0; i < hitsCount + 1; i++)
+        for (int i = 0; i <= hitsCount; i++)
         {
             Ray ray = new Ray(LastHitPosition, transform.forward);
 
@@ -41,57 +40,23 @@ public class RealRender2D : MonoBehaviour
             if (Physics.Raycast(ray, out hitInfo, rayLength))
             {
                 hitsCount++;
-                positions.Add(hitInfo.point);
-
+                newPositions.Add(hitInfo.point);
                 LastHitPosition = hitInfo.point + new Vector3(0, 0, 0.01f);
-
-                Debug.Log(positions[i]);
+                //Debug.Log(newPositions[i]);
                 // Dibujar el rayo en la escena hasta el punto de colisión
                 Debug.DrawRay(LastHitPosition, transform.forward * hitInfo.distance, Color.red);
-
             }
-
-            if (i % 2 == 0 && i != 0)
+           
+            if (i % 2 == 0 && i != 0 && lastPositions != newPositions)
             {
                 HitObjects colidedObject = new HitObjects();
-                GameObject nPlane = new GameObject();
-                nPlane = Instantiate(plane, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, -90));
-
-                colidedObject.initPosition = positions[i - 2];
-                colidedObject.endPosition = positions[i - 1];
-                colidedObject.nPlanes = nPlane;
-
-
-
+                colidedObject.initPosition = newPositions[i - 2];
+                colidedObject.endPosition = newPositions[i - 1];
                 hObjetcs.Add(colidedObject);
             }
 
         }
-
-        foreach (HitObjects item in hObjetcs)
-        {
-            float scale = hObjetcs[0].endPosition.z - hObjetcs[0].initPosition.z; 
-            float position = scale / 2;
-            hObjetcs[0].nPlanes.gameObject.layer = LayerMask.NameToLayer("2D");
-            hObjetcs[0].nPlanes.gameObject.transform.position = new Vector3(0, 0, hObjetcs[0].initPosition.z + position);
-            hObjetcs[0].nPlanes.gameObject.transform.localScale = new Vector3(plane.transform.localScale.x / 10, 1, scale / 10); //scale units 1 position = 10 scale
-            
-            //Destroy(nPlane.gameObject);
-            //hObjetcs.Clear();
-
-        }
-
-        // //calcular escala i posicio del pla
-        //    
-        // float scale = (positions[1].z - positions[0].z);
-        // float position = scale / 2;
-        //
-        //
-        //
-        // p.layer = LayerMask.NameToLayer("2D");
-        // p.transform.position = new Vector3(0, 0, positions[0].z + position);
-        // p.transform.localScale = new Vector3(plane.transform.localScale.x / 10, 1, scale / 10);
-        // instant++;
+        lastPositions = newPositions;
 
     }
 
---
+++ TFG-Dimensions-Game/Assets/Scripts/Word_Scripts/WorldGenerator.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static RealRender2D;
+
+public class WorldGenerator : MonoBehaviour
+{
+
+    public GameObject plane;
+    public RealRender2D rInfo;
+    List<GameObject> planeList = new ();
+
+    int numberObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (numberObjects != rInfo.hObjetcs.Count)
+        {
+
+            for (int i = 0; i < rInfo.hObjetcs.Count; i++)
+            {
+                GameObject newPlane = Instantiate(plane, Vector3.zero, Quaternion.Euler(0, 0, -90));
+                numberObjects++;
+                planeList.Add(newPlane);
+            }
+        }
+        for (int i = 0; i < planeList.Count; i++)
+        {
+            float scale = getObjectScale(i);
+            float position = getObjectPos2D(i);
+            UpdateFloor(i, scale, position);
+        }
+
+        //Destroy(nPlane.gameObject);
+        //hObjetcs.Clear();
+    }
+
+    private float getObjectScale (int numobj)
+    {
+        float scale = rInfo.hObjetcs[numobj].endPosition.z - rInfo.hObjetcs[numobj].initPosition.z;
+        return scale;
+    }
+    private float getObjectPos2D(float scale)
+    {
+        float position = scale / 2;
+        return position;
+    }
+    private void UpdateFloor(int number, float scale, float position)
+    {
+        planeList[number].layer = LayerMask.NameToLayer("2D");
+        planeList[number].transform.position = new Vector3(0, 0, rInfo.hObjetcs[0].initPosition.z + position);
+        planeList[number].transform.localScale = new Vector3(plane.transform.localScale.x / 10, 1, scale / 10); //scale units 1 position = 10 scale
+    }
+}
