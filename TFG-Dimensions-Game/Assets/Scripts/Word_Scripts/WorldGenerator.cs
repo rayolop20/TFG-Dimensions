3edfb4bf72ed0fa3872f89c88b857812cd1cb532
Pol Maresch <polmaresch@gmail.com>
2024-05-23 14:18:13 +0200

    Starting Y Calculations

--- TFG-Dimensions-Game/Assets/Scripts/Word_Scripts/WorldGenerator.cs
+++ TFG-Dimensions-Game/Assets/Scripts/Word_Scripts/WorldGenerator.cs
@@ -78,7 +78,7 @@ public class WorldGenerator : MonoBehaviour
     {
         planes.layer = LayerMask.NameToLayer("2D");
         planes.transform.position = new Vector3(rInfo.hObjetcs[number].initPosition.x + position.x, rInfo.hObjetcs[number].goGeneralVariables.transform.position.y, rInfo.hObjetcs[number].initPosition.z + position.z);
-        planes.transform.localScale = new Vector3(scale / 10, 1, rInfo.hObjetcs[number].goGeneralVariables.transform.localScale.y / 10); //scale units 1 position = 10 scale//new Vector3(scale / 10, 1, plane.transform.localScale.z / 10);
-        planes.transform.eulerAngles = new Vector3(planes.transform.eulerAngles.x, 0,rInfo.rotationPlayer.transform.eulerAngles.y) ; //new Vector3(scale / 10, 1, rInfo.hObjetcs[number].goScale.y / 10); //scale units 1 position = 10 scale
+        planes.transform.localScale = new Vector3(scale / 10, 1, rInfo.hObjetcs[number].goGeneralVariables.transform.localScale.y / 10); //scale units 1 position = 10 scale
+        planes.transform.eulerAngles = new Vector3(planes.transform.eulerAngles.x , 0,rInfo.rotationPlayer.transform.eulerAngles.y); //scale units 1 position = 10 scale  // rInfo.hObjetcs[number].goGeneralVariables.transform.eulerAngles.z
     }
 }
