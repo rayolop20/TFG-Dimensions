013d9a6c3ece12803d8c99b36f641d10b5c71062
Pol Maresch <polmaresch@gmail.com>
2024-06-18 13:30:50 +0200

    Fixed

--- TFG-Dimensions-Game/Assets/Scripts/Word_Scripts/WorldGenerator.cs
+++ TFG-Dimensions-Game/Assets/Scripts/Word_Scripts/WorldGenerator.cs
@@ -8,7 +8,8 @@ using UnityEngine.UIElements;
 public class WorldGenerator : MonoBehaviour
 {
     public GameObject plane;
-    public RayOrder rOrdered;
+    //public RayOrder rOrdered;
+    public RayLogic rayLogic;
     public Transform pInfo;
     Dictionary<int, GameObject> dictPlanes = new Dictionary<int, GameObject>();
     public SwapMaterial swapMaterial;
@@ -23,11 +24,11 @@ public class WorldGenerator : MonoBehaviour
     void Update()
     {
 
-        if (numberObjects != rOrdered.notRepItems.Count) // crerar numeror de objectes segons cada 2 punts
+        if (numberObjects != rayLogic.hObjetcs.Count) // crerar numeror de objectes segons cada 2 punts
         {
-            foreach (KeyValuePair<int, HitObjects> g in rOrdered.notRepItems)
+            foreach (KeyValuePair<int, HitObjects> g in rayLogic.hObjetcs)
             {
-                if (numberObjects < rOrdered.notRepItems.Count && !dictPlanes.ContainsKey(g.Key))
+                if (numberObjects < rayLogic.hObjetcs.Count && !dictPlanes.ContainsKey(g.Key))
                 {
                     GameObject newPlane = Instantiate(plane, Vector3.zero, plane.transform.rotation);
                     dictPlanes.Add(g.Key, newPlane);
@@ -36,11 +37,11 @@ public class WorldGenerator : MonoBehaviour
             }
         }
 
-        if (numberObjects > rOrdered.notRepItems.Count)
+        if (numberObjects > rayLogic.hObjetcs.Count)
         {
             foreach (KeyValuePair<int, GameObject> removed in dictPlanes)
             {
-                if (!rOrdered.notRepItems.ContainsKey(removed.Key))
+                if (!rayLogic.hObjetcs.ContainsKey(removed.Key))
                 {
                     Destroy(dictPlanes[removed.Key].gameObject);
                     dictPlanes.Remove(removed.Key);
@@ -50,13 +51,13 @@ public class WorldGenerator : MonoBehaviour
             }
         }
 
-        if (numberObjects == rOrdered.notRepItems.Count)
+        if (numberObjects == rayLogic.hObjetcs.Count)
         {
             foreach (KeyValuePair<int, GameObject> item in dictPlanes)
             {
                 int index = item.Key; // Clave del diccionario
                 GameObject plane = item.Value; //valor de dintre
-                plane.tag = rOrdered.notRepItems[index].goGeneralVariables.tag;
+                plane.tag = rayLogic.hObjetcs[index].goGeneralVariables.tag;
                 swapMaterial.SwapMat(plane, plane.tag);
 
                 Vector3 position = getObjectPos2D(index);
@@ -70,9 +71,9 @@ public class WorldGenerator : MonoBehaviour
     private Vector3 getObjectPos2D(int numobj)
     {
         Vector3 centralPoint = Vector3.zero;
-        if (rOrdered.notRepItems.ContainsKey(numobj))
+        if (rayLogic.hObjetcs.ContainsKey(numobj))
         {
-            Vector3 distanceBewteenPoints = rOrdered.notRepItems[numobj].endPosition - rOrdered.notRepItems[numobj].initPosition;
+            Vector3 distanceBewteenPoints = rayLogic.hObjetcs[numobj].endPosition - rayLogic.hObjetcs[numobj].initPosition;
             centralPoint = distanceBewteenPoints / 2;
         }
 
@@ -81,14 +82,14 @@ public class WorldGenerator : MonoBehaviour
     private float GetScale(int numobj)
     {
         //Formula Euclidiana
-        float Scale = Mathf.Sqrt(Mathf.Pow(rOrdered.notRepItems[numobj].endPosition.x - rOrdered.notRepItems[numobj].initPosition.x, 2) + Mathf.Pow(rOrdered.notRepItems[numobj].endPosition.z - rOrdered.notRepItems[numobj].initPosition.z, 2));
+        float Scale = Mathf.Sqrt(Mathf.Pow(rayLogic.hObjetcs[numobj].endPosition.x - rayLogic.hObjetcs[numobj].initPosition.x, 2) + Mathf.Pow(rayLogic.hObjetcs[numobj].endPosition.z - rayLogic.hObjetcs[numobj].initPosition.z, 2));
         return Scale;
     }
     private void UpdateFloor(GameObject planes, int number, float scale, Vector3 position)
     {
         planes.layer = LayerMask.NameToLayer("2D");
-        planes.transform.position = new Vector3(rOrdered.notRepItems[number].initPosition.x + position.x, rOrdered.notRepItems[number].goGeneralVariables.transform.position.y, rOrdered.notRepItems[number].initPosition.z + position.z);
-        planes.transform.localScale = new Vector3(scale / 10, 1, rOrdered.notRepItems[number].goGeneralVariables.transform.localScale.y / 10); //scale units 1 position = 10 scale
+        planes.transform.position = new Vector3(rayLogic.hObjetcs[number].initPosition.x + position.x, rayLogic.hObjetcs[number].goGeneralVariables.transform.position.y, rayLogic.hObjetcs[number].initPosition.z + position.z);
+        planes.transform.localScale = new Vector3(scale / 10, 1, rayLogic.hObjetcs[number].goGeneralVariables.transform.localScale.y / 10); //scale units 1 position = 10 scale
                                                                                                                                                // planes.transform.rotation = new Quaternion(planes.transform.rotation.x, planes.transform.rotation.y, rInfo.rotationPlayer.transform.rotation.z, planes.transform.rotation.w);
         planes.transform.eulerAngles = new Vector3(planes.transform.eulerAngles.x, 0, pInfo.transform.eulerAngles.y); //scale units 1 position = 10 scale  // rInfo.hObjetcs[number].goGeneralVariables.transform.eulerAngles.z
     }
